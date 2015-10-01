7ede346132ea2baf65bb8cf052f7ceff7c8ac2bf
Liang Xingchen <xuayee@gmail.com>
2015-10-01 19:02:54 +0800

    v1.0.0

---
+++ index.js
@@ -0,0 +1,50 @@
+/**
+ * 脉冲软件
+ * http://maichong.it
+ * Created by Liang on 15/10/1.
+ * liang@maichong.it
+ */
+
+'use strict';
+
+/**
+ * 检测号码是否为中国移动
+ * @param tel
+ * @returns {boolean}
+ */
+function isCMCC(tel) {
+	return /^(13[4-9]|15[012789]|18[23478]|178)\d+$/.test(tel);
+};
+
+/**
+ * 检测号码是否为中国电信
+ * @param tel
+ * @returns {boolean}
+ */
+function isCTCC(tel) {
+	return /^(133|153|180|181|189|177)\d+$/.test(tel);
+};
+
+/**
+ * 检测号码是否为中国联通
+ * @param tel
+ * @returns {boolean}
+ */
+function isCUCC(tel) {
+	return /^(13[012]|15[56]|18[56]|176)\d+$/.test(tel);
+};
+
+/**
+ * 检测号码是否为合法的中国手机号
+ * @param tel
+ * @returns {boolean}
+ */
+function verify(tel) {
+	return isCMCC(tel) || isCTCC(tel) || isCUCC(tel);
+};
+
+verify.isCMCC = isCMCC;
+verify.isCTCC = isCTCC;
+verify.isCUCC = isCUCC;
+
+module.exports = verify;
